1c4726db1d89f2ad86dd86641f67089087754e55
Brian Shoemaker <brianshoemaker@gmail.com>
2014-05-26 07:22:03 -0500

    Update to express v4.x

--- server.js
+++ server.js
@@ -4,29 +4,22 @@
  */
 
 var express = require('express');
-var routes = require('./routes');
+var bodyParser = require('body-parser');
+var compress = require('compression');
+var favicon = require('static-favicon');
 var http = require('http');
 var path = require('path');
+var routes = require('./routes');
 var c = require('./config').config;  // App configuration
 
 var app = express();
-
-// all environments
 app.set('port', process.env.PORT || c.appPort);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hjs');
-// app.use(express.favicon());
-// app.use(express.logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded());
-app.use(express.methodOverride());
-app.use(app.router);
+//app.use(favicon(__dirname + '/img/favicon.ico'));
+app.use(compress());
+app.use(bodyParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
-// development only
-if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
-}
 
 
 /**
